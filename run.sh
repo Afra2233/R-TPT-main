63e2bfa98f9ef95cbd2a082958cdabc86f8b6b87
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-12 20:35:25 +0100

    f

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=rtpt_clean
+#SBATCH -p gpu-medium
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=96G
+#SBATCH --cpus-per-task=8
+#SBATCH -o /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.out
+#SBATCH -e /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.err
+
+module add anaconda3/2022.05
+source activate rtpt
+python rtpt.py /scratch/hpc/07/zhang303/R-TPT-main/dataset --test_sets Caltech101 -a RN50 -b 64 --gpu 0 --ctx_init a_photo_of_a -p 50 --eps 0.0 --output_dir 'output_results/rtpt'

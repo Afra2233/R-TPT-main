7a848277dce7de8ef5709ca633c71e2f232feda1
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-12 23:36:47 +0100

    f

---
+++ run_ece.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=rtpt_clean
+#SBATCH -p gpu-medium
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:tesla_v100-pcie-32gb:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=96G
+#SBATCH --cpus-per-task=8
+#SBATCH -o /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.out
+#SBATCH -e /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.err
+
+module add anaconda3/2022.05
+source activate rtpt
+python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset --test_sets Caltech101  -a RN50 -b 64 --gpu 0 --ctx_init a_photo_of_a -p 50 --eps 0.0 --output_dir 'output_results/rtpt'
+# python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset --test_sets Caltech101 -a RN50 -b 64 --gpu 0 --ctx_init a_photo_of_a -p 50 --eps 1.0 --step 7 --output_dir 'output_results/rtpt'

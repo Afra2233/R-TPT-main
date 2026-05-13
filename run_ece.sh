b4f5cdd8891c7fe413e4f228db2e5d354942b2f9
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-13 19:27:05 +0100

    f

--- run_ece.sh
+++ run_ece.sh
@@ -30,7 +30,6 @@ source activate rtpt
 #   --dirichlet_weight \
 #   --dir_weight_beta 0.1 \
 #   --rtpt_tau 0.01
-
 python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
   --test_sets DTD \
   -a RN50 \
@@ -39,12 +38,15 @@ python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
   --ctx_init a_photo_of_a \
   -p 50 \
   --eps 0.0 \
-  --output_dir output_results/rtpt_dir_only_clean \
+  --output_dir output_results/rtpt_dir_evi_clean \
   --dirichlet_consistency \
   --lambda_tpt 1.0 \
   --lambda_dir 1.0 \
   --dir_temp 0.5 \
-  --alpha_offset 1.0
+  --alpha_offset 1.0 \
+  --evidence_penalty \
+  --lambda_evi 1e-4 \
+  --evidence_mode log_total
 
 # adv
 # python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset --test_sets eurosat  -a RN50 -b 64 --gpu 0 --ctx_init a_photo_of_a -p 50 --eps 1.0 --step 7 --output_dir 'output_results/rtpt'

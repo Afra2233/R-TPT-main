8b0cf84c414fdb94008f78176be43a6d4b3921c8
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-13 01:00:08 +0100

    f

--- run_ece.sh
+++ run_ece.sh
@@ -13,3 +13,6 @@ module add anaconda3/2022.05
 source activate rtpt
 # python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset --test_sets UCF101  -a RN50 -b 64 --gpu 0 --ctx_init a_photo_of_a -p 50 --eps 0.0 --output_dir 'output_results/rtpt'
 python rtpt_ece.py /scratch/hpc/07/zhang303/R-TPT-main/dataset --test_sets UCF101 -a RN50 -b 64 --gpu 0 --ctx_init a_photo_of_a -p 50 --eps 1.0 --step 7 --output_dir 'output_results/rtpt'
+
+# fewshot_datasets = ['DTD', 'Flower102', 'Food101', 'Cars', 'SUN397', 
+#                     'Aircraft', 'Pets', 'Caltech101', 'UCF101', 'eurosat']

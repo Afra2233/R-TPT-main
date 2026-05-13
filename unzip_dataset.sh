726d1815f68a01ba07ac2aeba530df7a675ac782
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-13 01:05:33 +0100

    f

--- unzip_dataset.sh
+++ unzip_dataset.sh
@@ -26,8 +26,8 @@ source activate rtpt
 echo "Job started on $(hostname)"
 echo "Start time: $(date)"
 
-DATA_DIR="/scratch/hpc/07/zhang303/R-TPT-main/dataset/ucf101"
-FILE_ID="1I0S0q91hJfsV9Gf4xDIjgDq4AqBNJb1y"
+DATA_DIR="/scratch/hpc/07/zhang303/R-TPT-main/dataset/dtd"
+FILE_ID="1u3_QfB467jqHgNXC00UIzbLZRQCg2S7x"
 
 mkdir -p "$DATA_DIR"
 cd "$DATA_DIR" || {

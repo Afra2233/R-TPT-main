b43f0cfa7a89ab8f23c012781e9821149455e674
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-14 12:25:09 +0100

    f

--- rtpt_a.sh
+++ rtpt_a.sh
@@ -12,25 +12,6 @@
 module add anaconda3/2022.05
 source activate rtpt
 
-python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets eurosat \
-  -a RN50 \
-  -b 64 \
-  --gpu 0 \
-  --ctx_init a_photo_of_a \
-  -p 1000 \
-  --eps 0.0 \
-  --output_dir output_results/dir_cons_entropy_clean \
-  --ece_bins 15 \
-  --high_conf_th 0.9 \
-  --dir_conservative_entropy \
-  --dir_temp 1.0 \
-  --alpha_offset 1.0 \
-  --dir_gate_tau 0.1 \
-  --lambda_cons 1.0
-
-
-# adv:
 # python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
 #   --test_sets eurosat \
 #   -a RN50 \
@@ -38,9 +19,8 @@ python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
 #   --gpu 0 \
 #   --ctx_init a_photo_of_a \
 #   -p 1000 \
-#   --eps 1.0 \
-#   --steps 7 \
-#   --output_dir output_results/dir_cons_entropy_robust \
+#   --eps 0.0 \
+#   --output_dir output_results/dir_cons_entropy_clean \
 #   --ece_bins 15 \
 #   --high_conf_th 0.9 \
 #   --dir_conservative_entropy \
@@ -48,6 +28,26 @@ python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
 #   --alpha_offset 1.0 \
 #   --dir_gate_tau 0.1 \
 #   --lambda_cons 1.0
+
+
+adv:
+python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
+  --test_sets eurosat \
+  -a RN50 \
+  -b 64 \
+  --gpu 0 \
+  --ctx_init a_photo_of_a \
+  -p 1000 \
+  --eps 1.0 \
+  --steps 7 \
+  --output_dir output_results/dir_cons_entropy_robust \
+  --ece_bins 15 \
+  --high_conf_th 0.9 \
+  --dir_conservative_entropy \
+  --dir_temp 1.0 \
+  --alpha_offset 1.0 \
+  --dir_gate_tau 0.1 \
+  --lambda_cons 1.0
 
 
   

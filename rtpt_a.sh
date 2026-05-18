f05cd77eacaaf38c0d157a77c47f6d77a0ca94de
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-18 15:25:19 +0100

    f

--- rtpt_a.sh
+++ rtpt_a.sh
@@ -43,21 +43,6 @@ source activate rtpt
 
 # adv：
 
-python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets DTD \
-  -a RN50 \
-  -b 64 \
-  --gpu 0 \
-  --ctx_init a_photo_of_a \
-  -p 1000 \
-  --eps 1.0 \
-  --steps 7 \
-  --tta_steps 0 \
-  --output_dir output_results/tta0_robust \
-  --ece_bins 15 \
-  --high_conf_th 0.9
-
-
 # python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
 #   --test_sets DTD \
 #   -a RN50 \
@@ -67,10 +52,25 @@ python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
 #   -p 1000 \
 #   --eps 1.0 \
 #   --steps 7 \
-#   --tta_steps 1 \
-#   --output_dir output_results/tta1_robust \
+#   --tta_steps 0 \
+#   --output_dir output_results/tta0_robust \
 #   --ece_bins 15 \
 #   --high_conf_th 0.9
+
+
+python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
+  --test_sets DTD \
+  -a RN50 \
+  -b 64 \
+  --gpu 0 \
+  --ctx_init a_photo_of_a \
+  -p 1000 \
+  --eps 1.0 \
+  --steps 7 \
+  --tta_steps 1 \
+  --output_dir output_results/tta1_robust \
+  --ece_bins 15 \
+  --high_conf_th 0.9
 
 # works====================================================================
 # python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \

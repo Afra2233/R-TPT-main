916af01037b4aaf0c5603d06ed492728754fe632
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-21 23:21:08 +0100

    g

--- rtpt_a.sh
+++ rtpt_a.sh
@@ -15,7 +15,7 @@ source activate rtpt
 
 # works====================================================================
 python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets UCF101 \
+  --test_sets DTD \
   -a RN50 \
   -b 64 \
   --gpu 0 \

5314175a67d777a36df2de65f96ff87f28c9cb98
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-21 13:14:14 +0100

    f

--- rtpt_o.sh
+++ rtpt_o.sh
@@ -9,20 +9,8 @@
 #SBATCH -o /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.out
 #SBATCH -e /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.err
 
-# module add anaconda3/2022.05
-# source activate rtpt
-
-# python rtpt_o.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-#   --test_sets eurosat \
-#   -a RN50 \
-#   -b 64 \
-#   --gpu 0 \
-#   --ctx_init a_photo_of_a \
-#   -p 1000 \
-#   --eps 0.0 \
-#   --output_dir output_results/rtpt_otpt_clean \
-#   --otpt \
-#   --lambda_otpt 18
+module add anaconda3/2022.05
+source activate rtpt
 
 python rtpt_o.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
   --test_sets eurosat \
@@ -31,8 +19,20 @@ python rtpt_o.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
   --gpu 0 \
   --ctx_init a_photo_of_a \
   -p 1000 \
-  --eps 1.0 \
-  --steps 7 \
-  --output_dir output_results/rtpt_otpt_robust \
+  --eps 0.0 \
+  --output_dir output_results/rtpt_otpt_clean \
   --otpt \
   --lambda_otpt 18
+
+# python rtpt_o.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
+#   --test_sets eurosat \
+#   -a RN50 \
+#   -b 64 \
+#   --gpu 0 \
+#   --ctx_init a_photo_of_a \
+#   -p 1000 \
+#   --eps 1.0 \
+#   --steps 7 \
+#   --output_dir output_results/rtpt_otpt_robust \
+#   --otpt \
+#   --lambda_otpt 18

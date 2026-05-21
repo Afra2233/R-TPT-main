67040cb9f84d7c1c332a059704d0aee48ba15052
afra818 <60651664+afra818@users.noreply.github.com>
2026-05-21 23:22:04 +0100

    f

--- rtpt_a.sh
+++ rtpt_a.sh
@@ -15,7 +15,7 @@ source activate rtpt
 
 # works====================================================================
 python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets DTD \
+  --test_sets eurosat \
   -a RN50 \
   -b 64 \
   --gpu 0 \
@@ -34,7 +34,7 @@ python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
 
 
 # python rtpt_a.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-#   --test_sets UCF101 \
+#   --test_sets eurosat \
 #   -a RN50 \
 #   -b 64 \
 #   --gpu 0 \

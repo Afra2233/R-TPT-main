ed0276bb4c9edf64591799fb5a1148a998d116b5
afra818 <60651664+afra818@users.noreply.github.com>
2026-07-15 18:15:39 +0100

    f

--- rtpt_pure_clibration.sh
+++ rtpt_pure_clibration.sh
@@ -16,13 +16,13 @@ source activate rtpt
 
 # ===================== step 0==============================
 python rtpt_pure_clibration.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets SUN397 \
+  --test_sets DTD \
   -a RN50 \
   -b 64 \
   --gpu 0 \
   --ctx_init a_photo_of_a \
   -p 50 \
-  --eps 0.0 \
+  --eps 1.0 \
   --tta_steps 0 \
   --output_dir output_results/pure_rtpt_calibration
 

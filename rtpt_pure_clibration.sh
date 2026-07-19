b33e5fc9a7353296430d386b315b67c9af53f840
afra818 <60651664+afra818@users.noreply.github.com>
2026-07-19 14:22:53 +0100

    f

--- rtpt_pure_clibration.sh
+++ rtpt_pure_clibration.sh
@@ -28,7 +28,7 @@ source activate rtpt
 
   # ===================== step 1 ==============================
   python rtpt_pure_clibration.py /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets Pets \
+  --test_sets Aircraft \
   -a RN50 \
   -b 64 \
   --gpu 0 \

d693686c03b98464d6852458b86e1da054911b0a
afra818 <60651664+afra818@users.noreply.github.com>
2026-08-02 15:54:36 +0100

    f

--- rtpt_temerature.sh
+++ rtpt_temerature.sh
@@ -16,7 +16,7 @@ cd /scratch/hpc/07/zhang303/R-TPT-main
 
 python rtpt_temperature.py \
   /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets DTD \
+  --test_sets Caltech101 \
   -a RN50 \
   -b 64 \
   --gpu 0 \

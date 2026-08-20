2ef814e37894a11ae1630c254497230b07b61069
afra818 <60651664+afra818@users.noreply.github.com>
2026-08-20 21:52:58 +0100

    f

---
+++ rtpt_adaptive_with_uniform_rtpt_diagnostics.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#SBATCH --job-name=rtpt_diag
+#SBATCH -p gpu-medium
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:tesla_v100-pcie-32gb:1
+#SBATCH --time=48:00:00
+#SBATCH --mem=96G
+#SBATCH --cpus-per-task=8
+#SBATCH -o /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.out
+#SBATCH -e /scratch/hpc/07/zhang303/R-TPT-main/%x-%j.err
+
+module add anaconda3/2022.05
+source activate rtpt
+
+cd /scratch/hpc/07/zhang303/R-TPT-main
+
+python rtpt_adaptive_with_uniform_rtpt_diagnostics.py \
+  /scratch/hpc/07/zhang303/R-TPT-main/dataset \
+  --test_sets DTD \
+  -a RN50 \
+  -b 64 \
+  --gpu 0 \
+  --ctx_init a_photo_of_a \
+  -p 50 \
+  --eps 1.0 \
+  --tta_steps 1 \
+  --steps 7 \
+  --rtpt_tau 0.01 \
+  --high_conf_th 0.9 \
+  --output_dir output_results/uniform_rtpt_diagnostics

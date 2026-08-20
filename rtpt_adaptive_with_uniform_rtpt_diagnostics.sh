7462d2e469d7eaa5077501b5261d1f5787ffa030
afra818 <60651664+afra818@users.noreply.github.com>
2026-08-21 00:15:03 +0100

    f

--- rtpt_adaptive_with_uniform_rtpt_diagnostics.sh
+++ rtpt_adaptive_with_uniform_rtpt_diagnostics.sh
@@ -17,7 +17,7 @@ cd /scratch/hpc/07/zhang303/R-TPT-main
 
 python rtpt_adaptive_with_uniform_rtpt_diagnostics.py \
   /scratch/hpc/07/zhang303/R-TPT-main/dataset \
-  --test_sets Flower102\
+  --test_sets Pets\
   -a RN50 \
   -b 64 \
   --gpu 0 \

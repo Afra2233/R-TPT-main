2a7026a5c2ffe0cd441bda494f5f600b9c7eb702
afra818 <60651664+afra818@users.noreply.github.com>
2026-08-27 14:59:16 +0100

    f

--- rtpt_lambda05_temperature_nll_brier.sh
+++ rtpt_lambda05_temperature_nll_brier.sh
@@ -14,7 +14,7 @@ source activate rtpt
 	# DTD	Caltech101	UCF101	Flower102	Pets	Aircraft	Food101	Pets	Aircraft	eurosat	Cars	SUN397
 python rtpt_lambda05_temperature_nll_brier.py \
 /scratch/hpc/07/zhang303/R-TPT-main/dataset \
---test_sets Flower102 \
+--test_sets Pets \
 -a RN50 \
 -b 64 \
 --gpu 0 \
